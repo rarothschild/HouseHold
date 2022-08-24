10ffdd490c39596f1bebe2274f49d6b4b19e9fbd
rarothschild <rarothsc@gmail.com>
2022-08-24 12:02:24 -0700

    Restarting backend, adding django-rest urls

--- backend/houses/apps.py
+++ backend/houses/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HouseholdConfig(AppConfig):
+class HousesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HouseHold'
+    name = 'houses'
